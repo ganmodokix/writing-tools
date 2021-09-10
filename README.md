f631bf419beb86cb1afb92ad88878c13b68c20aa
Ganmodokix <lugerp07@live.jp>
2021-09-10 17:10:29 +0900

    rewrited mistakenly deleted tex2text.html

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # writing-tools
 little tools for writing
+
+## usage
+open in your browser
+all of them works locally
+
+## textabilify.html
+copy and paste a CSV table to convert into a TeX table
+
+## wordcounter.html
+copy and paste some sentences to count how many words it has
+
+# tex2text.html
+copy and paste a passage of your TeX document and convert it to a plain text
---
+++ tex2text.js
@@ -0,0 +1,47 @@
+function convert(text) {
+
+    const text_lines = text.split(/\r?\n/g);
+
+    const converted_lines = [];
+
+    for (let line of text_lines) {
+
+        // remove lines without anything but comments
+        if (line.match(/^\s*%.*/)) {
+            continue;
+        }
+
+        // remove a comment at the ending of the line
+        line = line.replace(/\s*%.*$/, "");
+
+        // replace citations with dummy one
+        // e.g. the previous studies~\cite{Foobars1919}. -> the previous studies [42]
+        line = line.replace(/~?\\cite{.*?}/g, "[42]");
+
+        // replace references with dummy one
+        // e.g. Just look at Fig.~\ref{fig:hoge}. -> Just look at Fig. 42.
+        line = line.replace(/~?\\ref{.*?}/g, "42");
+
+        converted_lines.push(line);
+
+    }
+
+    return converted_lines.join("\n");
+
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const textareaIn = document.querySelector("textarea#in");
+    const textareaOut = document.querySelector("textarea#out");
+
+    textareaIn.addEventListener("keyup", () => {
+
+        const rawTeX = textareaIn.value;
+        const convertedText = convert(rawTeX);
+
+        textareaOut.value = convertedText;
+
+    });
+
+}, {once: true})
